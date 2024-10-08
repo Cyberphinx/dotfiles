b449e38756fe0314704fad6fd8b0114592f990fb
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-10-08 20:15:48 +0100

    feat: arandr

---
+++ arandr/asus_zenbook.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 3640x840 --rotate normal --output DP-1 --mode 3840x1100 --pos 1720x1920 --rotate normal --output DP-2 --mode 1920x1080 --pos 2560x0 --rotate right --output HDMI-1-0 --primary --mode 2560x1440 --pos 0x480 --rotate normal
